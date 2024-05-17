41aace50cca35cc37a733ff3a970824898572660
VladCojo <vladvcojocaru@gmail.com>
2024-05-17 14:01:32 +0300

    created linkedlist struct

--- linked-list/main.c
+++ linked-list/main.c
@@ -7,6 +7,11 @@ typedef struct node{
     struct node *next;
 }node_t;
 
+typedef struct {
+    int length;
+    node_t *head;
+}list;
+
 
 node_t *create_node(int data){
     node_t *newNode = (node_t*) malloc(sizeof (node_t));
@@ -23,9 +28,15 @@ node_t *create_node(int data){
 }
 
 
-void append_node(node_t **head, int data){
+void append_node(list **list, int data){
     node_t *newNode = create_node(data);
-    node_t *tmp = *head;
+
+    if((*list)->head == NULL){
+        (*list)->head = newNode;
+        return;
+    }
+
+    node_t *tmp = (*list)->head;
 
     while (tmp->next) {
         tmp = tmp->next;
@@ -34,15 +45,20 @@ void append_node(node_t **head, int data){
 
 }
 
-void push_node(node_t **head, int data){
+void push_node(list **list, int data){
     node_t *newNode = create_node(data);
-    newNode->next = *head;
-    *head = newNode;
+    newNode->next = (*list)->head;
+    (*list)->head = newNode;
 
 }
 
-void display_data(node_t **head){
-    node_t *tmp = *head;
+void display_data(list **list){
+    if((*list)->head == NULL){
+        printf("The list is empty\n");
+        return;
+    }
+
+    node_t *tmp = (*list)->head;
 
     while (tmp){
         printf("%d ", tmp->data);
@@ -50,12 +66,22 @@ void display_data(node_t **head){
     }
 }
 
-void delete_node(node_t **head, int data){
-    node_t *tmp = *head;
+void delete_node(list **list, int data){
+    if((*list)->head == NULL){
+        printf("The list is empty");
+        return;
+    }
+
+    node_t *tmp = (*list)->head;
 
     while (tmp->next->data != data){
         tmp = tmp->next;
     }
+
+    if(tmp->next == NULL){
+        printf("The node does not exist");
+    }
+
     node_t *del = tmp->next;
     tmp->next=tmp->next->next;
     del->next = NULL;
@@ -64,13 +90,18 @@ void delete_node(node_t **head, int data){
 }
 
 int main() {
-    node_t *head = create_node(20);
-    append_node(&head, 10);
-    push_node(&head, 30);
-    display_data(&head);
-    delete_node(&head, 20);
-    
-    display_data(&head);
+    list *myList = (list *)malloc(sizeof(list));
+    display_data(&myList);
+
+
+    for(int i = 0; i <= 20; i ++){
+        append_node(&myList, i);
+    }
+
+    display_data(&myList);
+    printf("\n");
+    delete_node(&myList, 19);
+    display_data(&myList);
 
     return 0;
 }
