5fc2d7de467767e6e8ac0088670edda9c76d77c9
VladCojo <vladvcojocaru@gmail.com>
2024-05-18 16:27:15 +0300

    finished delete node function for dll

--- double-linked-list/main.c
+++ double-linked-list/main.c
@@ -18,22 +18,74 @@ node_t *create_node(int data);
 void push(list_t **list, int data);
 void append(list_t **list, int data);
 void display(list_t **list);
-node_t *delete(list_t **list, int data);
+node_t *delete_node(list_t **list, int data);
 
 
 
 int main() {
     list_t *list = (list_t *) malloc(sizeof (list_t));
     push(&list, 10);
-    push(&list, 12);
+    push(&list, 155);
     append(&list, 9);
     append(&list, 12);
     push(&list, 23);
     display(&list);
-    printf("Length: %d", (*list).length);
+    printf("Length: %d\n", list->length);
+    delete_node(&list, 23);
+    display(&list);
+    printf("Length: %d\n", list->length);
 
 
     return 0;
+}
+
+node_t *delete_node(list_t **list, int data){
+    // check if list is empty
+    if((*list)->head == NULL){
+        printf("Cannot delete node because list is null\n");
+        return NULL;
+    }
+
+    node_t *tmp = (*list)->head;
+
+    // check if we have to delete the first node
+   if(tmp->data == data){
+       (*list)->head = tmp->next;
+       if((*list)->head != NULL){
+           (*list)->head->prev = NULL;
+       } else {
+           (*list)->tail = NULL;
+       }
+       tmp->next = NULL;
+       (*list)->length--;
+       return tmp;
+   }
+    // traverse the list until we find the node or get to the end of the list
+    while (tmp && tmp->data != data){
+        tmp = tmp->next;
+    }
+
+    // check if the node doesn't exist
+    if(tmp == NULL){
+        printf("The node doesn't exist\n");
+        return NULL;
+    }
+
+    // check if the node is the tail
+    if(tmp == (*list)->tail){
+        (*list)->tail = tmp->prev;
+        (*list)->tail->next = NULL;
+        (*list)->length--;
+        return tmp;
+    }
+
+    // delete node
+    node_t *prev = tmp->prev;
+    prev->next = tmp->next;
+    tmp->next = NULL;
+    (*list)->length--;
+    return tmp;
+
 }
 
 void display(list_t **list){
