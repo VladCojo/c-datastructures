ee39cc97ee1ab0210a33d214ac7e766210d1252e
VladCojo <vladvcojocaru@gmail.com>
2024-05-18 13:52:26 +0300

    started merge sort algo

--- linked-list/main.c
+++ linked-list/main.c
@@ -14,6 +14,48 @@ typedef struct {
 }list;
 
 
+node_t *create_node(int data);
+void display_data(list **list);
+void append_node(list **list, int data);
+void push_node(list **list, int data);
+node_t *delete_node(list **list, int data);
+void merge(list **list, int l, int r, int m);
+void mergeSort(list **list, int l, int r);
+void splitList(list **list, node_t **frontRef, node_t **backRef);
+// TODO: Create insert node function
+
+
+
+int main() {
+    list *myList = (list *)malloc(sizeof(list));
+    push_node(&myList, 5);
+    append_node(&myList, 3);
+    display_data(&myList);
+
+    return 0;
+}
+
+void splitList(list **list, node_t **frontRef, node_t **backRef){
+    node_t *fast;
+    node_t *slow;
+    slow = (*list)->head;
+    fast = (*list)->head->next;
+
+    while (fast->next != NULL){
+        fast = fast->next;
+        if(fast->next != NULL){
+            fast = fast->next;
+            slow = slow->next;
+        }
+    }
+
+    *frontRef = (*list)->head;
+    *backRef = slow->next;
+
+    slow->next = NULL;
+}
+
+
 node_t *create_node(int data){
     node_t *newNode = (node_t*) malloc(sizeof (node_t));
 
@@ -112,18 +154,4 @@ node_t *delete_node(list **list, int data){
     (*list)->length--;
 
     return tmp;
-
-
-}
-
-// TODO: Create insert node function
-
-
-
-int main() {
-    list *myList = (list *)malloc(sizeof(list));
-    push_node(&myList, 20);
-    delete_node(&myList, 20);
-
-    return 0;
 }
